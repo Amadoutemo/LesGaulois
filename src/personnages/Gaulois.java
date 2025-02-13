74ec68617390712b68f22fb1aa6ce4e6f0587310
Amadoutemo <amadou.temo@gmail.com>
2025-02-13 11:59:12 +0100

    Commit romain gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,23 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	
+	public Gaulois(String nom, int force) {
+		this.force = force;
+		this.nom = nom;
+	
+	}
+	public String getNom ()  {
+		return nom;
+	}	
+	public void parler(String texte) {
+		// TODO Auto-generated method stub
+
+	}
+	
+		// TODO Auto-generated constructor stub
+	}
+
+}
